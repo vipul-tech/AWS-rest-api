87ccf64de7b4b7418223f7099381da5c12c67f78
kumavip <kumarvipul1513@gmail.com>
2024-09-17 13:37:15 +0530

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (id, description, done, target_date,username)
+values (10001, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10002, 'Get AWS Certified', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10004, 'Learn Python', true, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10005, 'Learn Machine Learning', true, CURRENT_DATE(), 'in28minutes');
